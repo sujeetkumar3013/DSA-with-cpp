2cf0c02bdb01ecebdd397ba85a8a244053ce05d0
The writer sujeet <thewritersujeet@Thes-MacBook-Air.local>
2026-08-11 15:30:11 +0530

    Add LCM program

---
+++ lcm.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, lcm;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    lcm = (a > b) ? a : b;
+
+    while (true)
+    {
+        if (lcm % a == 0 && lcm % b == 0)
+        {
+            break;
+        }
+
+        lcm++;
+    }
+
+    cout << "LCM = " << lcm;
+
+    return 0;
+}
