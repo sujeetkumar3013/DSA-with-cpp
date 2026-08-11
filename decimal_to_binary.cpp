286b626f071ce74c73ae8b4de050cab67aec84c3
The writer sujeet <thewritersujeet@Thes-MacBook-Air.local>
2026-08-11 15:59:52 +0530

    Add Decimal to Binary program

---
+++ decimal_to_binary.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, binary[32], i = 0;
+
+    cout << "Enter a decimal number: ";
+    cin >> n;
+
+    while (n > 0)
+    {
+        binary[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+
+    cout << "Binary = ";
+
+    for (i = i - 1; i >= 0; i--)
+    {
+        cout << binary[i];
+    }
+
+    return 0;
+}
