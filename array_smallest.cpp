d03cad40d54bb77ce535f7d404ac5eccaac2641a
The writer sujeet <thewritersujeet@Thes-MacBook-Air.local>
2026-08-13 12:18:19 +0530

    Add Array Smallest Element program

---
+++ array_smallest.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " elements: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int smallest = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] < smallest)
+        {
+            smallest = arr[i];
+        }
+    }
+
+    cout << "Smallest element = " << smallest;
+
+    return 0;
+}
