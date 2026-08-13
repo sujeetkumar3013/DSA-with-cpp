0e2a58e34027f3564f5a0573c14b2b7af1774d4e
The writer sujeet <thewritersujeet@Thes-MacBook-Air.local>
2026-08-13 12:09:01 +0530

    Add Array Input Output program

---
+++ array_input_output.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " elements: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Array elements: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
