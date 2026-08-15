fce1d279062cbdacc0d369da6cdcbc01c97196c4
The writer sujeet <thewritersujeet@Thes-MacBook-Air.local>
2026-08-15 15:35:25 +0530

    Add Array Left Rotation program

---
+++ array_left_rotate.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    cout << "Enter " << n << " elements: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int first = arr[0];
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+
+    arr[n - 1] = first;
+
+    cout << "Array after left rotation: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
