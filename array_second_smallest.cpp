0c957b681208a46ed022f48b9a0750cfee2473ef
The writer sujeet <thewritersujeet@Thes-MacBook-Air.local>
2026-08-15 15:11:05 +0530

    Add Second Smallest Element program

---
+++ array_second_smallest.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " elements: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int smallest = INT_MAX;
+    int secondSmallest = INT_MAX;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < smallest)
+        {
+            secondSmallest = smallest;
+            smallest = arr[i];
+        }
+        else if (arr[i] < secondSmallest && arr[i] != smallest)
+        {
+            secondSmallest = arr[i];
+        }
+    }
+
+    if (secondSmallest == INT_MAX)
+        cout << "Second smallest element does not exist";
+    else
+        cout << "Second smallest element = " << secondSmallest;
+
+    return 0;
+}
