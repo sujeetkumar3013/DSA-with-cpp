e520994c23d7c9c291526d4f0d4a40deedb3bbe8
The writer sujeet <thewritersujeet@Thes-MacBook-Air.local>
2026-08-18 22:23:18 +0530

    Add remove duplicates program

---
+++ remove_duplicates_array.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << "Array after removing duplicates: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        bool duplicate = false;
+
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                duplicate = true;
+                break;
+            }
+        }
+
+        if (!duplicate)
+            cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
