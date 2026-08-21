7e8b336172db5a3a5d67d9318fcb4650b81b5fdd
The writer sujeet <thewritersujeet@Thes-MacBook-Air.local>
2026-08-21 14:29:36 +0530

    Added average array program

---
+++ average_array.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+    int sum = 0;
+
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    double average = (double)sum / n;
+
+    cout << "Average = " << average << endl;
+
+    return 0;
+}
