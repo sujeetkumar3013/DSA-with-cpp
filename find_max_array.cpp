64a60c2953cb97f2b381ff6b920358562704fbec
The writer sujeet <thewritersujeet@Thes-MacBook-Air.local>
2026-08-21 14:22:26 +0530

    Added find max array program

---
+++ find_max_array.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int max = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    cout << "Maximum element = " << max << endl;
+
+    return 0;
+}
